b39835431922fd1142628935e510bb9ffc524b9e
nurhussenm <nurhussenm80@gmail.com>
2025-12-31 18:39:23 +0300

    route for projects

---
+++ src/routes/projectRoutes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { getProjects, createProject } from "../controllers/projectController.js";
+
+const router = Router();
+
+router.get('/', getProjects);
+
+
+router.post('/', createProject);
+
+export default router;
