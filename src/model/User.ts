170d2c1dc8a4225af5bee89684333d26cd990e19
nurhussenm <nurhussenm80@gmail.com>
2026-01-03 19:01:34 +0300

    bcrypt created for auth

---
+++ src/model/User.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema, Document } from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+export interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+}
+
+const UserSchema: Schema = new Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+// The "Magic Blender": Hash password before saving
+UserSchema.pre<IUser>('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+export default mongoose.model<IUser>('User', UserSchema);
